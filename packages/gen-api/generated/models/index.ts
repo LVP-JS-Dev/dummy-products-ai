0e5805da70eec0fe71d55b092429120daf623f94
Leonid Petrov <lvpjsdev@gmail.com>
2026-02-11 09:02:23 +0300

    gen-api: add DummyJSON OpenAPI client + react-query hooks

---
+++ packages/gen-api/generated/models/index.ts
@@ -0,0 +1,8 @@
+export type { ErrorResponse } from "./ErrorResponse.ts";
+export type { ListProductsQueryParams, ListProducts200, ListProducts400, ListProducts401, ListProducts408, ListProducts500, ListProductsQueryResponse, ListProductsQuery } from "./ListProducts.ts";
+export type { Login200, Login400, Login401, Login408, Login500, LoginMutationRequest, LoginMutationResponse, LoginMutation } from "./Login.ts";
+export type { LoginRequest } from "./LoginRequest.ts";
+export type { LoginResponse } from "./LoginResponse.ts";
+export type { Product } from "./Product.ts";
+export type { ProductPage } from "./ProductPage.ts";
+export type { SearchProductsQueryParams, SearchProducts200, SearchProducts400, SearchProducts401, SearchProducts408, SearchProducts500, SearchProductsQueryResponse, SearchProductsQuery } from "./SearchProducts.ts";
